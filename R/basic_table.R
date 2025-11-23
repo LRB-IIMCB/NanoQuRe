d0c5ef8166e7d047e317e8f28ebb2027c5d7423f
All-ice0 <ala.stachurska04@gmail.com>
2025-11-24 00:45:25 +0100

    seqsum_database

--- R/basic_table.R
+++ R/basic_table.R
@@ -1,6 +1,13 @@
 
-basic_table <- function(ligase_data){
-  ligase_data %>%
+ligase_ela <- read_tsv("/mnt/nanopore/raw_data/libraries_tests/250320_ligaseEla_SuperScript/20250320_1344_MN39398_FBA13144_c7450e82/sequencing_summary_FBA13144_c7450e82_7c2ba15e.txt")
+
+ligase_induro <- read_tsv("/mnt/nanopore/raw_data/libraries_tests/250320_ligaseT4_Induro/20250320_1337_MN40838_FBA13273_4849cee3/sequencing_summary_FBA13273_4849cee3_5ab4411b.txt")
+
+seqsum_database = list(ligase_ela, ligase_induro)
+
+
+basic_table <- function(my_data){
+  my_data %>%
   summarise(
   "run id" = first(run_id),
   "duration" = max(start_time + duration, na.rm = TRUE) / 3600,
@@ -8,5 +15,15 @@ basic_table <- function(ligase_data){
   )
 }
 
-pro_table <- basic_table(ligase_data)
+for (n in seqsum_database)
+{
+  basic_table(n)
 
+}
+
+
+"""
+table_ela <- basic_table(ligase_ela)
+table_induro <- basic_table(ligase_induro)
+
+"""
