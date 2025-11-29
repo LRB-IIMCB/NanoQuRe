5e854d6281784c0b51a1ea72a97631bbe43bfd13
All-ice0 <ala.stachurska04@gmail.com>
2025-11-29 01:05:15 +0100

    comments

--- R/bases_per_seq_length.R
+++ R/bases_per_seq_length.R
@@ -2,16 +2,17 @@
 #BiocManager::install("ShortRead")
 #library(ShortRead)
 
-
+# calculating mean length of reads
 mean_length_ela = mean(ligase_ela$sequence_length_template)
 
+# calculating n50 - 50% of the total length of the genome assembly
 n50_value <- Biostrings::N50(ligase_ela$sequence_length_template)
 
 
-
+# counting occurrence of every read's length
 count_seq <- length_ascd %>% dplyr::count(sequence_length_template)
 
-
+# creating a plot comparing reads length and their occurrence
 chart_ela <- ggplot(data = count_seq, aes(x=sequence_length_template, y=n))
 
 
--- R/basic_table.R
+++ R/basic_table.R
@@ -1,8 +1,10 @@
-
+#sample data - ligase
 ligase_ela <- read_tsv("/mnt/nanopore/raw_data/libraries_tests/250320_ligaseEla_SuperScript/20250320_1344_MN39398_FBA13144_c7450e82/sequencing_summary_FBA13144_c7450e82_7c2ba15e.txt")
 
 ligase_induro <- read_tsv("/mnt/nanopore/raw_data/libraries_tests/250320_ligaseT4_Induro/20250320_1337_MN40838_FBA13273_4849cee3/sequencing_summary_FBA13273_4849cee3_5ab4411b.txt")
 
+
+# creating list of sequencing summary files
 seqsum_database = list(ligase_ela, ligase_induro)
 
 #assertthat::`%has_name%`()
@@ -16,10 +18,12 @@ seqsum_database = list(ligase_ela, ligase_induro)
 #'
 #' @examples
 basic_table <- function(my_data){
+  #checking data
   assertthat::not_empty(my_data$run_id)
   assertthat::not_empty(my_data$start_time)
   assertthat::not_empty(my_data$duration)
 
+  # creating table with sequencing parameters
   my_data %>%
   summarise(
   "run id" = first(run_id),
@@ -28,9 +32,10 @@ basic_table <- function(my_data){
   )
 }
 
+# applying basic_table function to every element in the seqencing summary database
 btable_database_lapply <- lapply(seqsum_database, basic_table)
 
 
 
-#assertthat::assert_that(assertive::has_rows(samples_table),msg = "Empty data frame provided as an input (samples_table). Please provide samples_table describing data to load")
+
 
