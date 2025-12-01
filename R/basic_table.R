d7110a49870b5e00e6dbbf6a319b961f290a419a
All-ice0 <ala.stachurska04@gmail.com>
2025-12-01 23:28:28 +0100

    plotly table based on seq sum

--- R/basic_table.R
+++ R/basic_table.R
@@ -38,24 +38,69 @@ basic_table <- function(my_data){
 }
 
 
-
-
 # applying basic_table function to every element in the seqencing summary database
-btable_database_lapply <- lapply(seqsum_database, basic_table)
 
-tab1 <- ligase_ela %>%
-  summarise(
-    "run id" = first(run_id),
-    "duration" = max(start_time + duration, na.rm = TRUE) / 3600,
-    "number of reads" = n())
 
-tab2 <- plot_ly(
-  type = 'table',
-  header = list(
-    values = c("run id", "duration", "number of reads")),
-  cells = list(
-    values = t(tab1))
-)
+plotlytab_database = list()
 
-print(tab2)
+plotly_table <- function(my_data){
+  for (n in 1:length(seqsum_database)){
+    #print(n)
+    #print(seqsum_database[n])
+    #new_tab <- as.data.frame(seqsum_database[n])
+    tab1 <- seqsum_database[[n]] %>%
+      summarise(
+        "run id" = first(run_id),
+        "duration" = max(start_time + duration, na.rm = TRUE) / 3600,
+        "number of reads" = n())
+    #plotlytab_database <- append(tab1, plotlytab_database)
+    plotlytab_database <- c(plotlytab_database, list(tab1))
+    print(plotlytab_database)
+  }
+  joinedtab <- dplyr::bind_rows(plotlytab_database)
+
+  print(joinedtab)
+
+
+  joinedtab <-  dplyr::bind_rows(plotlytab_database)
+  print(joinedtab)
+
+  tab2 <- plot_ly(
+    type = 'table',
+    columnwidth = c(200, 50),
+    columnorder = c(0, 1),
+    header = list(
+      values = c("run id", "duration", "number of reads"),
+      line = list(width = 1, color = 'black'),
+      fill = list(color = c("royalblue", "royalblue")),
+      font = list(family = "Arial", size = 14, color = "white")),
+    cells = list(
+      values = t(joinedtab),
+      line = list(width = 1, color = 'black'))
+  )
+}
+
+
+plotly_database_lapply <- lapply(seqsum_database, plotly_table)
+
+print(plotly_database_lapply)
+
+
+for (n in 1:length(seqsum_database)){
+  #print(n)
+  #print(seqsum_database[n])
+  #new_tab <- as.data.frame(seqsum_database[n])
+  tab1 <- seqsum_database[[n]] %>%
+    summarise(
+      "run id" = first(run_id),
+      "duration" = max(start_time + duration, na.rm = TRUE) / 3600,
+      "number of reads" = n())
+  #plotlytab_database <- append(tab1, plotlytab_database)
+  plotlytab_database <- c(plotlytab_database, list(tab1))
+  print(plotlytab_database)
+}
+joinedtab <- dplyr::bind_rows(plotlytab_database)
+
+print(joinedtab)
+
 
